d4f528739ef95bb72c936a265ff63ff2d4d55fcd
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-15 09:55:52 -0700

    Add tables to database via migration.

--- db/migrate/20150515163752_add_bands_venues_tables_and_join.rb
+++ db/migrate/20150515163752_add_bands_venues_tables_and_join.rb
@@ -1,4 +1,24 @@
 class AddBandsVenuesTablesAndJoin < ActiveRecord::Migration
   def change
+    create_table :bands do |t|
+      t.column :name, :string
+
+      t.timestamps()
+    end
+
+    create_table :venues do |t|
+      t.column :name, :string
+      t.column :location, :string
+      
+      t.timestamps()
+    end
+
+    create_table :bands_venues do |t|
+      t.integer :band_id
+      t.integer :venue_id
+    end
+
+    add_index :bands_venues, :band_id
+    add_index :bands_venues, :venue_id
   end
 end
