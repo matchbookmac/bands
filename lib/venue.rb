3fa73a8221e9d0a6b7a55347baf0275012a13ca2
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-15 12:06:57 -0700

    Add titleize and downcase methods to venues for validation.

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,4 +1,14 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many :bands
   validates :name, {presence: true, uniqueness: {case_sensitive: false}}
+  before_validation :titleize_venue
+
+  private
+  def titleize_venue
+    self.name = Maybe(name).downcase
+    self.name = Maybe(name).titleize
+    self.location = Maybe(location).downcase
+    self.location = Maybe(location).titleize
+# binding.pry
+  end
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper'
 
-describe Band do
-  it { should have_and_belong_to_many :venues }
+describe Venue do
+  it { should have_and_belong_to_many :bands }
 
-  # it { should validate_presence_of :name}
+  it { should validate_presence_of :name}
 
   it { should validate_uniqueness_of :name}
+
+  it 'titleizes the name of the venue' do
+    venue = Venue.create name: 'kingston mines'
+    expect(venue.name).to eq 'Kingston Mines'
+  end
+
+  it 'titleizes the location of the venue' do
+    venue = Venue.create name: 'kingston mines', location: 'chicago'
+    expect(venue.location).to eq 'Chicago'
+  end
 end
