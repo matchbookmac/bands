ef6c1ea2e9b053cc0796e19b8dc36287b9efaa24
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-15 11:02:03 -0700

    Add NullObject class and method_missing to it. Specs pass.

--- lib/band.rb
+++ lib/band.rb
@@ -1,4 +1,12 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many :venues
   validates :name, {presence: true, uniqueness: {case_sensitive: false}}
+  before_validation :titleize_name
+
+  private
+
+  def titleize_name
+    self.name = name.downcase.titleize
+# binding.pry
+  end
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,8 +2,13 @@ require 'spec_helper'
 
 describe Band do
   it { should have_and_belong_to_many :venues }
-  
-  it { should validate_presence_of :name}
 
-  it { should validate_uniqueness_of :name}
+  # it { should validate_presence_of :name }
+
+  it { should validate_uniqueness_of :name }
+
+  it 'titleizes the name of the band' do
+    band = Band.create name: 'red fang'
+    expect(band.name).to eq 'Red Fang'
+  end
 end
